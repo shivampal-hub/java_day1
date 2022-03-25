e634bf431379b5c2c5dd870d4172281c6a81e87b
shivampal-hub <70017082+shivampal-hub@users.noreply.github.com>
2022-03-26 01:16:25 +0530

    Update NonStatic.java

--- NonStatic.java
+++ NonStatic.java
@@ -1,7 +1,7 @@
 public class NonStatic {
     int a = 3, b = 4;
     public static void main(String[] args){
-        //non-static var,method.],obj cant directly access in static method,need to create object, in this case method add cant be accessed directly in main class due to which object is created.
+        //non-static var,method cant directly access in static method,need to create object, in this case method add cant be accessed directly in main class due to which object is created.
         NonStatic S = new NonStatic();
         S.add();
     }
